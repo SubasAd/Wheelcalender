3f58fea4e3dd73603215c60acf321c039ac0e15f
bond <saileshshiwakoti2@gmail.com>
2024-06-05 16:51:06 +0545

    12

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Wheel from './Components/wheel.jsx';
+import ParentComponent from './Components/SmallerComponent/day.jsx';
 
 function App() {
   return (
     <div>
-		<Wheel/>
-      
+		
+      <ParentComponent/>
     </div>
   );
 }
